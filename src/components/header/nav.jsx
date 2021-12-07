345e66a247a013d30f0e9b61a302deb7500855d2
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-07 16:42:46 +0100

    add nav links and sign up/sign in buttons

--- src/components/header/nav.jsx
+++ src/components/header/nav.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Nav = ()=> {
     return (
-        <div>
-            <h1>Navigation</h1>
-        </div>
+        <nav className="nav">
+            <ul>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+            <div className="right">
+                <a href="">Sign Up</a>
+                <a href="#" className="btn">Sign In</a>
+            </div>
+        </nav>
     )
 }
 
