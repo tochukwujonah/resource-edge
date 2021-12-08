1655edfd5b4b8ab25deaa2486c06c686b0a422ab
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-08 10:23:58 +0100

    create partners-section-component

---
+++ src/components/main/partners-section/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PartnersSectionComponent = ()=> {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PartnersSectionComponent;
