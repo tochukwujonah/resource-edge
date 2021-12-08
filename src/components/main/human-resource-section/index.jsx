d683d6b8a0f74efe49e7748edde35c9aca083d19
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-08 10:22:02 +0100

    rename human-reource to human-resource-section-component

--- src/components/main/human-resource-section/index.jsx
+++ src/components/main/human-resource-section/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const HumanResource = ()=> {
+const HumanResourceSectionComponent = ()=> {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const HumanResource = ()=> {
     )
 }
 
-export default HumanResource;
+export default HumanResourceSectionComponent;
