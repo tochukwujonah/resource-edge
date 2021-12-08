d217653eb3e0f73d78f1de36c838b3f3fedc42ee
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-08 08:49:53 +0100

    add caption and illustration components

--- src/components/showcase/index.jsx
+++ src/components/showcase/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Illustration from './illustration'
+import { Caption } from '../global-components' 
 
 const Showcase = ()=> {
     return (
-        <div>
+        <section className="showcase">
+            <div className="container showcase-container">
+                <Caption title="Throw paperwork into the trash where it belongs." body="Eliminate all the hassle involved in managing people operations by automating it." />
+
+                <Illustration />
+            </div>
             
-            
-        </div>
+        </section>
     )
 }
 
