3ddf21cd0ff6ccf8f0507ae3617bbafaa40e2309
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-08 10:23:21 +0100

    create logistics-section-component

---
+++ src/components/main/logistics-section/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LogisticsSectionComponent = ()=> {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LogisticsSectionComponent;
