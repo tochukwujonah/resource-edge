d684d643de7f7877def4dc5c72be21b205868a72
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-09 08:31:16 +0100

    import iframe component to resource-section component

--- src/components/main/human-resource-section/index.jsx
+++ src/components/main/human-resource-section/index.jsx
@@ -3,6 +3,7 @@ import { Caption, Card } from '../../global-components'
 import team from '../../../assets/icons/team.svg'
 import calender from '../../../assets/icons/calender.svg'
 import goal from '../../../assets/icons/goal.svg'
+import IframeComponent from '../../iframe-component'
 
 const HumanResourceSectionComponent = ()=> {
     return (
@@ -12,13 +13,17 @@ const HumanResourceSectionComponent = ()=> {
                 <Caption title="Human Resources" body="Onboard new employees, manage the employee lifecycle and measure employee performance." />
 
                 <div className="cards">
-                    <Card icon={team} color="#5243AA" title="Employee Management" body="From Hiring & Onboarding to Retiring, the Resource Edge Employee Management module  eliminates all the complexities & paperwork involved in managing your team." />
+                    <Card icon={team} color="#5243AA" title="Employee Management" body="From Hiring & Onboarding to Retiring, the Resource Edge Employee Management module  eliminates all the complexities & paperwork involved in managing your team." hasShadow = {false}/>
 
                     <Card icon={goal} color="#008DA6" title="Performance Management" body="Manage and track employee performance with our easy-to-use tools for goal setting, performance agreements and performance reviews." />
 
-                    <Card icon={calender} color="#42526E" title="Paid time off" body="Employees can request for paid time off, vacations, sick leaves or educational leaves and get approvals all within Resource Edge. HR managers can equally ensure compliance." />
+                    <Card icon={calender} color="#42526E" title="Paid time off" body="Employees can request for paid time off, vacations, sick leaves or educational leaves and get approvals all within Resource Edge. HR managers can equally ensure compliance." hasShadow = {false}/>
 
                 </div>
+
+                {/* My iframe component for employee directory goes here */}
+                <IframeComponent />
+                
             </div>
 
             
