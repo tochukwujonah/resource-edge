46acd443b03104db193e531f208396ed25e6800e
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-09 12:39:26 +0100

    add travel and logistics card to logistics-section-component

--- src/components/main/logistics-section/index.jsx
+++ src/components/main/logistics-section/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Caption, Card } from '../../global-components'
+
+import sent from '../../../assets/icons/sent.svg'
 
 const LogisticsSectionComponent = ()=> {
     return (
-        <div>
+        <div className="main-component-item">
+            <div className="container">
+                <div className="top">
+                    <Caption title="Admin & Logistics" body="Manage and track company assets as well as logistics for travelling employees"/>
+
+                    <Card icon={sent} color="#FF8B00" title="Travel & Logistics" body="Make travel requests, get approvals, and have access to travel information.
+                    " />
+                </div>
+            </div>
             
         </div>
     )
