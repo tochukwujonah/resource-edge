686601e8bc668666cffa5dc9c2ae3468275d212c
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-09 12:49:58 +0100

    style logistics section

--- src/components/main/logistics-section/index.jsx
+++ src/components/main/logistics-section/index.jsx
@@ -6,16 +6,16 @@ import briefcase from '../../../assets/icons/briefcase.svg'
 
 const LogisticsSectionComponent = ()=> {
     return (
-        <div className="main-component-item">
+        <div className="main-component-item logistics-section">
             <div className="container">
                 <div className="top">
                     <Caption title="Admin & Logistics" body="Manage and track company assets as well as logistics for travelling employees"/>
 
                     <Card icon={sent} color="#FF8B00" title="Travel & Logistics" body="Make travel requests, get approvals, and have access to travel information.
-                    " />
+                    " hasShadow={false} />
 
                     <Card icon={briefcase} color="#008DA6" title="Asset Management" body="Manage the acquisition, assignment, and disposition of assets seamlessly.
-                    " />
+                    " hasShadow={false} />
                 </div>
             </div>
             
