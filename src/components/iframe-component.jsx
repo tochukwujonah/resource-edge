539f9ecdc0406338d5b9df2900f727e14155aed4
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-09 10:05:34 +0100

    add main section to iframe component

--- src/components/iframe-component.jsx
+++ src/components/iframe-component.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import logo from '../assets/images/logo.png'
+import notification from '../assets/icons/notification.svg'
+import avatar from '../assets/images/staffs/right-content.png'
+import search from '../assets/icons/search.png';
 
 const IframeComponent = ()=> {
     return (
@@ -11,9 +15,36 @@ const IframeComponent = ()=> {
                         <span></span>
                     </div>
 
+                    <div className="logo">
+                        <img src={logo} />
+                    </div>
                     
+                    <h3>Employee Directory</h3>
+                </div>
+
+                <div className="right">
+                    <img src={notification} />
+                    <img src={avatar} />
                 </div>
             </header>
+
+            <main className="iframe-main-section">
+                <div className="container">
+                    <span className="neutral-bar"></span>
+                    <h3>Employees</h3>
+
+                    <div className="search-section">
+                        <form>
+                            <input placeholder="search" />
+                        </form>
+                        <img src={search} />
+                    </div>
+                </div>
+
+
+
+
+            </main>
             
         </div>
     )
