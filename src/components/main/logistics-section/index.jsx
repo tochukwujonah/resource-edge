c9d363ad1bef4e1c7e5a464ce7efdddf60ef813e
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-09 12:42:04 +0100

    add asset management card to logistics-section-component

--- src/components/main/logistics-section/index.jsx
+++ src/components/main/logistics-section/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Caption, Card } from '../../global-components'
 
 import sent from '../../../assets/icons/sent.svg'
+import briefcase from '../../../assets/icons/briefcase.svg'
 
 const LogisticsSectionComponent = ()=> {
     return (
@@ -11,6 +12,9 @@ const LogisticsSectionComponent = ()=> {
                     <Caption title="Admin & Logistics" body="Manage and track company assets as well as logistics for travelling employees"/>
 
                     <Card icon={sent} color="#FF8B00" title="Travel & Logistics" body="Make travel requests, get approvals, and have access to travel information.
+                    " />
+
+                    <Card icon={briefcase} color="#008DA6" title="Asset Management" body="Manage the acquisition, assignment, and disposition of assets seamlessly.
                     " />
                 </div>
             </div>
