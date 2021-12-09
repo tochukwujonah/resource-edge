aa72531e54d57bbd43f287756281d70257a353a3
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-09 12:16:05 +0100

    add logistics-section-component to main component

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import HumanResourceSectionComponent from './human-resource-section'
+import LogisticsSectionComponent from './logistics-section'
 
 const Main = ()=> {
     return (
         <main className="main-component">
             <HumanResourceSectionComponent />
-
+            <LogisticsSectionComponent />
             
         </main>
     )
