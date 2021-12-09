9c24d6c85d24eaefd32b71b6bbff7cf039f2aad6
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-09 08:33:04 +0100

    import and add showcase and main components to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Header from './components/header';
+import Showcase from './components/showcase';
+import Main from './components/main';
 
 const App = ()=> {
   return (
     <div className="App">
+      {/* My header section */}
       <Header />
+
+      {/* My showcase section */}
+      <Showcase />
+
+      {/* My main section */}
+      <Main />
       
     </div>
   );
