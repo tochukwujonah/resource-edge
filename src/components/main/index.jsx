52043402ac3fbadc42c9fd4df7a3c96f945237cc
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-10 01:33:40 +0100

    add finance-section-component to main component

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HumanResourceSectionComponent from './human-resource-section'
 import LogisticsSectionComponent from './logistics-section'
+import FinanceSectionComponent from './finance-section'
 
 const Main = ()=> {
     return (
         <main className="main-component">
             <HumanResourceSectionComponent />
             <LogisticsSectionComponent />
+            <FinanceSectionComponent />
             
         </main>
     )
