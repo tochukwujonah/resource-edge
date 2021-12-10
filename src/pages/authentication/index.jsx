ce7ea6783cc975f34bd74a34817ae8c1ecf8b61d
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-10 11:43:16 +0100

    add illustrations to auth page

--- src/pages/authentication/index.jsx
+++ src/pages/authentication/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Loginman from '../../assets/illustration/Loginman.png';
+import LogManWoman from '../../assets/illustration/LogManWoman.png';
 
 const AuthPage = ()=> {
     return (
-        <div>
+        <div className="page">
 
-            
+            <div className="page-illustration">
+                <img src={Loginman} />
+                <img src={LogManWoman} />
+            </div>
+
+            <div className="box">
+                
+            </div>
             
         </div>
     )
