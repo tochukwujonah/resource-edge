4cb71d2d9a10e83dc2c1e59a6d93260e0166839b
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-10 22:31:41 +0100

    create reset-password page

---
+++ src/pages/authentication/reset-password.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Page from '../Page'
+
+const ResetPasswordPage = ()=> {
+    return <Page title="Reset Password" caption="To enable us reset your password, enter your email below" />
+}
+
+const ChildComponent = ()=> {
+    return (
+        <></>
+    )
+}
+
+export default ResetPasswordPage
