047740f1849022233e74bb3f0ab10f36ea5bfef3
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-10 03:43:00 +0100

    add partners image to partners section

--- src/components/main/partners-section/index.jsx
+++ src/components/main/partners-section/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import unn from '../../../assets/images/partners/unn.png'
+import tenece from '../../../assets/images/partners/tenece.png'
+import estate from '../../../assets/images/partners/private-estate.png'
+import genesys from '../../../assets/images/partners/genesys.png'
+
 const PartnersSectionComponent = ()=> {
     return (
-        <div>
+        <div className="container partners">
+            <img src={ unn } alt="University of Nigeria Nsukka" />
+            <img src={ tenece } alt="Tenece" />
+            <img src={ estate } alt="Private Estate" />
+            <img src={ genesys } alt="Genesys" />
             
         </div>
     )
