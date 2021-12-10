b4bb24204d0e67dae31b931e003e4cc10c6bf222
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-10 12:02:52 +0100

    add logo to auth page

--- src/pages/authentication/index.jsx
+++ src/pages/authentication/index.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import Loginman from '../../assets/illustration/Loginman.png';
 import LogManWoman from '../../assets/illustration/LogManWoman.png';
+import logo from '../../assets/images/text_logo.png';
 
 const AuthPage = ()=> {
     return (
         <div className="page">
 
             <div className="page-illustration">
-                <img src={Loginman} />
-                <img src={LogManWoman} />
+                <img src={Loginman} style={{"--shift": "-200"}} />
+                <img src={LogManWoman} style={{"--shift": "200"}} />
             </div>
 
+            {/* My logo */}
+            <img src={logo} alt="Logo" />
+            
             <div className="box">
-                
+
             </div>
             
         </div>
