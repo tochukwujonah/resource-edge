0cdeb8276c25977426ec4c79993b0d6cc1345b73
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-10 13:32:29 +0100

    create state and event handlers for auth UI

--- src/pages/authentication/index.jsx
+++ src/pages/authentication/index.jsx
@@ -6,11 +6,29 @@ import logo from '../../assets/images/text_logo.png';
 const AuthPage = ()=> {
 
 
-    const user = useState({email: '', password: ''});
+    const [user, setUser] = useState({email: '', password: ''});
+    const [isValid, setIsValid] = useState(false);
+    const [progress, setProgress] = useState(0); //Monitors progress, 1 is forward, -1 is backward 
 
     const validateInput = (e)=> {
+        if(e.target.type === 'email'){
+            setUser({...user, email: e.target.value});
+            const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]+))$/;
+            
+            if(regex.test(user.email)) setIsValid(true);
+            else setIsValid(false);
+            
+        } else {
 
+            setUser({...user, password: e.target.value});
+        }     
+        
     }
+
+
+    const nextStep = ()=> setProgress(1);
+
+    const backStep = ()=> setProgress(-1);
 
     return (
         <div className="page">
@@ -29,12 +47,12 @@ const AuthPage = ()=> {
 
                 {/* My form */}
                 <form className="form-control">
-                    <div className="form-item">
+                    <div className={progress === 1 ? "form-item backward" : "form-item forward"}>
                         <label>Email Address</label>
                         <input type="email" placeholder="Enter email" value={user.email} onChange={validateInput}/>
                     </div>
                     <div className="form-item">
-                        <button className="inactive-btn">Next</button>
+                        <button className={ isValid ? "active-btn" : "inactive-btn"} onClick={nextStep}>Next</button>
                     </div>
                 </form>
 
