4cb95eb0e7f0911484e95fb634513a69c0b97eed
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-12 13:54:46 +0100

    create the dasjboard header section

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import avatar from '../../assets/icons/avatar.png';
 
 const Dashboard = ()=> {
+    const params = useParams();
+
     return (
-        <div>
-            
+        <div className="dashboard">
+            <header className="header dashboard-header">
+               <div className="header-left">
+                <div className="hamburger-menu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+
+                    <span className="logo">resource edge</span>
+
+                    <hr className="vertical-hr" />
+
+                    <span>TM Dashboard</span>
+               </div>
+
+               <div className="header-right">
+                   <img src={avatar} alt="Avatar"/>
+               </div>
+
+                
+            </header>
+
+            <main>
+
+                <div className="container dashboard-container">
+                    <h1>Hello, {params.user}</h1>
+                    <h5>Welcome and good to have you back</h5>
+                    <br />
+
+                    <h3>Things to do</h3>
+                </div>
+
+            </main>
             
         </div>
     )
