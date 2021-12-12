2517d5a6cf2c66bdfac5fe2003101b84d674c980
tochukwujonah <jonahtochukwu@gmail.com>
2021-12-12 21:56:06 +0100

    create mini hamburger menu

--- src/pages/landing/components/header/index.jsx
+++ src/pages/landing/components/header/index.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import Nav from './nav'
 
+import logo from '../../../../assets/images/logo.png'
+
 const Header = ()=> {
     return (
         <header className="header">
-            <div className="container main-header">
-                <span className="logo">resource edge</span>
+            <div className="container main-header macro">
+                <span className="logo logo-desk">resource edge</span>
 
                 {/* Navigation component belongs here */}
                 <Nav />
             </div>
+
+        <div className="main-header-mini mini">
+            <div className="container main-header">
+                <div className="logo-mini-container">
+                    <span className="logo-mini"><img src={logo} alt="Logo" /></span>
+                </div>
+                        
+                <div className="hamburger-menu hamburger-mini mini">
+                    <span style={{"--w": "1"}}></span>
+                    <span style={{"--w": ".75"}}></span>
+                    <span style={{"-w": ".5"}}></span>
+                </div>
+            </div>
+
+        </div>
+            
         </header>
     )
 }
